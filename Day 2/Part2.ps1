d896f11646e70ee177ca45f9815723f6beb79bec
John Gruss <john.gruss.4@gmail.com>
2021-12-02 08:06:12 -0500

    Part 2 complete

--- Day 2/Part2.ps1
+++ Day 2/Part2.ps1
@@ -1,3 +1,17 @@
 $source = get-content "$psscriptroot\input.txt"
 
-$array = 
+$array = @(0,0,0)
+
+$source | foreach-object {
+    $command = $_.split(" ")
+    switch -regex ($command[0]) {
+        "forward" {
+            $array[0] += $command[1]
+            $array[1] += ([int]$command[1] * [int]$array[2])
+        }
+        "up" {$array[2] -= $command[1]}
+        "down"{$array[2] += $command[1]}
+    }
+
+}
+$array[0] * $array[1]
